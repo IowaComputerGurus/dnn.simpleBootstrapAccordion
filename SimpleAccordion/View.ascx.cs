34af75b5ee6fbfb03c107fb51a7cdbb85b682816
Mitchel Sellers <msellers@iowacomputergurus.com>
2017-03-23 00:36:58 -0500

    UI Enhancement Release
    OSI-3: Added warning message if no panels defined
    OSI-4: Corrected missing text for "Add New Panel" link
    OSI-5: Edit Operation should note "Edit Panel"
    OSI-6: Edit Panel action is now showing as a button
    OSI-7: Resolve issue preventing caching of rendered content
    OSI-8: Changed accordion panel to default to full clickable

--- SimpleAccordion/View.ascx.cs
+++ SimpleAccordion/View.ascx.cs
@@ -4,6 +4,7 @@ using DotNetNuke.Entities.Modules;
 using DotNetNuke.Entities.Modules.Actions;
 using DotNetNuke.Security;
 using DotNetNuke.Services.Cache;
+using DotNetNuke.UI.Skins.Controls;
 using ICG.Modules.SimpleBootstrapAccordion.Components.Repositories;
 using ICG.Modules.SimpleBootstrapAccordion.Components.Utility;
 
@@ -40,12 +41,12 @@ namespace ICG.Modules.SimpleBootstrapAccordion
 
                 if (IsEditable)
                 {
-                    editLink = "<p><a href=\"" + ModuleContext.EditUrl("pci", "-1", "EditContent") + "\">Edit Panel</a></p>";
+                    editLink = $"<p><a href=\"{ModuleContext.EditUrl("pci", "-1", "EditContent")}\" class=\"btn btn-primary\">{LocalizeString("EditPanel")}</a></p>";
                 }
 
                 //Check for cached content
-                var cachedContent = CachingProvider.Instance().GetItem("ICG_BST_" + ModuleId);
-                if (cachedContent != null && !IsEditable)
+                var cachedContent = CachingProvider.Instance().GetItem("ICG_BSA_" + ModuleId);
+                if (cachedContent != null && !ModuleContext.IsEditable)
                 {
                     litContent.Text = cachedContent.ToString();
                 }
@@ -69,23 +70,31 @@ namespace ICG.Modules.SimpleBootstrapAccordion
                             panelBuilder.Append(itemTemplate);
                             panelBuilder.Replace("[PANELCONTENTID]", item.PanelContentId.ToString());
                             panelBuilder.Replace("[EXPANDED]", isFirstItem.ToString().ToLowerInvariant());
-                            panelBuilder.Replace("[HEADERCLASSATTRIBUTE]", isFirstItem ? string.Empty : "class=\"collapsed\"");
+                            panelBuilder.Replace("[HEADERCLASSATTRIBUTE]",
+                                isFirstItem ? string.Empty : "class=\"collapsed\"");
                             panelBuilder.Replace("[PANELTITLE]", item.PanelTitle);
                             panelBuilder.Replace("[PANELCONTENTCLASS]", isFirstItem ? " in" : string.Empty);
                             panelBuilder.Replace("[PANELCONTENT]", item.PanelContents);
                             panelBuilder.Replace("[EDITLINK]", editLink.Replace("-1", item.PanelContentId.ToString()));
                             isFirstItem = false;
                         }
+
+                        //Build the final one
+                        var finalBuilder = new StringBuilder(wrapperContent);
+                        finalBuilder.Replace("[ITEMS]", panelBuilder.ToString());
+                        finalBuilder.Replace("[ACCORDIONID]", accordionId);
+                        finalBuilder.Replace("[MULTISELECT]", multiSelect);
+                        litContent.Text = finalBuilder.ToString();
+                        if (!IsEditable)
+                            CachingProvider.Instance().Insert("ICG_BSA_" + ModuleId, litContent.Text);
+                    }
+                    else
+                    {
+                        DotNetNuke.UI.Skins.Skin.AddModuleMessage(this, LocalizeString("NotItems"),
+                            ModuleMessage.ModuleMessageType.BlueInfo);
                     }
 
-                    //Build the final one
-                    var finalBuilder = new StringBuilder(wrapperContent);
-                    finalBuilder.Replace("[ITEMS]", panelBuilder.ToString());
-                    finalBuilder.Replace("[ACCORDIONID]", accordionId);
-                    finalBuilder.Replace("[MULTISELECT]", multiSelect);
-                    litContent.Text = finalBuilder.ToString();
-                    if (!IsEditable)
-                        CachingProvider.Instance().Insert("ICG_BSA_" + ModuleId, litContent.Text);
+                    
                 }
             }
         }
